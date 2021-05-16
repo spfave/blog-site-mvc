1b5b4654748673c47c4402d9143ad08f79a3bf8a
Sebastian F <sebpfave2@gmail.com>
2021-05-15 23:42:35 -0400

    Create user seed data

--- db/seed/index.js
+++ db/seed/index.js
@@ -1,11 +1,15 @@
 const sequelize = require("../../config/connection");
-const { Post } = require("../../models");
+const { Post, User } = require("../../models");
 
+const userData = require("./seed-user.json");
 const postData = require("./seed-posts.json");
 
 async function seedDatabase() {
   await sequelize.sync({ force: true });
   console.log("\n----- DATABASE SYNCED -----\n");
+
+  await User.bulkCreate(userData, { individualHooks: true });
+  console.log("\n----- USERS SEEDED -----\n");
 
   await Post.bulkCreate(postData);
   console.log("\n----- POSTS SEEDED -----\n");
