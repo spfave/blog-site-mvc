b996fd3608ded1649c8540fe7ffea9b51e4abcdc
Sebastian F <sebpfave2@gmail.com>
2021-05-15 23:46:38 -0400

    Create User - Post model relations

--- models/index.js
+++ models/index.js
@@ -1,3 +1,9 @@
+const User = require("./User");
 const Post = require("./Post");
 
-module.exports = { Post };
+// User-Post relations
+User.hasMany(Post, { foreignKey: "user_id", onDelete: "CASCADE" });
+
+Post.belongsTo(User, { foreignKey: "user_id" });
+
+module.exports = { User, Post };
