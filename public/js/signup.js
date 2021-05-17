4dbb6f3e3213fd8514d718c35775e78e1f19c9b0
Sebastian F <sebpfave2@gmail.com>
2021-05-16 23:13:51 -0400

    Create signup client side code

---
+++ public/js/signup.js
@@ -0,0 +1,25 @@
+const handleFormSignup = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username").value.trim();
+  const password = document.querySelector("#password").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users/signup", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/"); //dashboard
+    } else {
+      const test = await response.json();
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector("#form-signup")
+  .addEventListener("click", handleFormSignup);
