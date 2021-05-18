3d5f2f41a0a877e6c44a8bc1ae84bc6eeaeae125
Sebastian F <sebpfave2@gmail.com>
2021-05-18 17:32:06 -0400

    Update model relations

--- models/index.js
+++ models/index.js
@@ -1,9 +1,20 @@
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
 
 // User-Post relations
-User.hasMany(Post, { foreignKey: "user_id", onDelete: "CASCADE" });
+User.hasMany(Post, { foreignKey: "user_id" });
 
 Post.belongsTo(User, { foreignKey: "user_id" });
 
-module.exports = { User, Post };
+// User-Comment relations
+User.hasMany(Comment, { foreignKey: "user_id" });
+
+Comment.belongsTo(User, { foreignKey: "user_id" });
+
+// Post-Comment relations
+Post.hasMany(Comment, { foreignKey: "post_id", onDelete: "CASCADE" });
+
+Comment.belongsTo(Post, { foreignKey: "post_id" });
+
+module.exports = { User, Post, Comment };
