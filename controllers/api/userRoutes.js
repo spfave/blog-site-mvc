527dea88daab7784210e9071a90e30718a3c6393
Sebastian F <sebpfave2@gmail.com>
2021-05-17 20:19:21 -0400

    Create user login post route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -27,4 +27,36 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password, please try again" });
+      return;
+    }
+
+    const validatePassword = await userData.checkPassword(req.body.password);
+    if (!validatePassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (error) {
+    res.json(400).json(error);
+  }
+});
+
 module.exports = router;
