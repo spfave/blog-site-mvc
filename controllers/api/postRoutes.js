2f0ddebfe7b7e194f8b6d2152f1fb1ff58ed839e
Sebastian F <sebpfave2@gmail.com>
2021-05-18 22:41:48 -0400

    Create post POST route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,7 +3,18 @@ const { Post } = require("../../models");
 const checkAuth = require("../../utils/auth");
 
 // Create new post
-router.post("/", checkAuth, async (req, res) => {});
+router.post("/", checkAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 // Update post
 router.put("/:id", checkAuth, async (req, res) => {});
